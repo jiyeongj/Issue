1c2b56a5ea3008fb7e0f9a4e26b86fad54b620de
최지영 <jiyeong.choe@mirero.co.kr>
2019-12-11 17:29:53 +0900

    수정

--- AkkaCluster_Repeat_Deployment/Repeat_Deployment_Test/NonSeedNode2/test.bat
+++ AkkaCluster_Repeat_Deployment/Repeat_Deployment_Test/NonSeedNode2/test.bat
@@ -1,16 +1,16 @@
 :loop
 SET /a a+=1
-IF %a%==30 GOTO break
+IF %a%==50 GOTO break
 
 TASKKILL /im NonSeedNode2.exe /t
 
-TIMEOUT 5
+TIMEOUT 3
 
 SET CURDIR="%~dp0%"
 
 START /d %CURDIR%bin\Release\ NonSeedNode2.exe
 
-TIMEOUT 5
+TIMEOUT 3
 
 GOTO loop
 
